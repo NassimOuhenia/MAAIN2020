3dc2ed0f5b78a75bd33fce8f06d1b0b0ef09ae16
Nassim <nassim.ouhenia@yahoo.com>
2020-01-29 16:54:17 +0100

    out = wiki_musique.xml

--- src/parse_xml.py
+++ src/parse_xml.py
@@ -6,7 +6,7 @@ import xml.etree.ElementTree as ET
 file = 'frwikidebut.xml'
 mot = 'musique'
 pref = '{http://www.mediawiki.org/xml/export-0.10/}'
-out = 'wiki_musique.fr'
+out = 'wiki_musique.xml'
 
 #exo 1
 def reduire_mot_cle(name_file, mot_cle, out):
