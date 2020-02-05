e64f4697b38d7efa539fe7d0a5141ab6474caa6e
andressito <andrekeny@gmail.com>
2020-02-05 11:14:36 +0100

    nettoyage

--- src/parse_xml.py
+++ src/parse_xml.py
@@ -3,6 +3,10 @@
 
 import xml.etree.ElementTree as ET
 import re
+from nltk.corpus import stopwords
+from nltk.tokenize import sent_tokenize, word_tokenize
+import nltk
+#nltk.download('punkt')
 
 file = 'frwikidebut.xml'
 mot = 'musique'
@@ -10,16 +14,27 @@ pref = '{http://www.mediawiki.org/xml/export-0.10/}'
 out = 'wiki_musique.xml'
 
 #exo 1
-def reduire_mot_cle(name_file, mot_cle, out):
 
+def reduire_mot_cle(name_file, mots, out):
     tree = ET.parse(name_file)
     root = tree.getroot()
-
+    music=0
+    i=0
     for page in root.findall(pref+'page'):
         txt = page.find(pref+'revision/'+pref+'text').text
-        if mot not in txt:
+        txt= txt.lower()
+        music=0
+        for mot in mots:
+            if mot in txt:
+                music+=1
+        if music ==0:
             root.remove(page)
-    tree.write(out)
+    tree.write(out,encoding="utf-8", xml_declaration=None, default_namespace=None, method="xml")
+    print("ok")
+
+def testReduire():
+    mots3=["infobox musique"]
+    print(reduire_mot_cle2("/home/andressito/Bureau/M2/semestre2/MAAIN/TP1/frwiki.xml",mots3,"outputFinal.xml"))
 
 def replace_all(text, dic):
     for i, j in dic.items():
@@ -27,47 +42,45 @@ def replace_all(text, dic):
     return text
 
 def cleaning (string):
-    #string = re.sub('de$','e',string)
-    #string = string.strip('-=')
-    string = re.sub('&#233;','e',string)   # è
-    string = re.sub('la?e?s?$',' ',string)    # j'ai fait un truc ca ne marche plus
-    #string = re.sub('=+',' ',string)     # les =
+    string= string.lower()
+    string = re.sub('\s+',' ',string)
     string = re.sub('<[^>]+>', '',string) # les balises
+    string = re.sub('\[*.*?\]', '',string) # les balises
+    string = re.sub('\{\{.*?\}\}', '',string)
+    string = re.sub('=+.*?=+', '',string)
+    string = re.sub('\(*.*?\)', '',string)
+    string = re.sub('&lt;.*?&gt;', '',string)
+    string = re.sub(r'https?:\/\/.*[\r\n]*', '', string, flags=re.MULTILINE)
+    string = re.sub('\*',' ',string)
+    string = re.sub('\W',' ',string)
+    #print(string)
     return string
 
 def remove_tags(text):
     return ''.join(ET.fromstring(text).itertext())
 
+def ouvrir2efile():
+    tree = ET.parse("outfevrier.xml")
+    root = tree.getroot()
+    for page in root.findall(pref+'page'):
+        txt = page.find(pref+'revision/'+pref+'text').text
+        print(cleaning(txt))
+    print("ok")
 
-def dictionnaire_de_mot(name_file):
-    dic={"===":"","le":"","la":"","de":"","un":"","une":"","}}":"",}
-    for line in open(name_file,"r"):
-        #line=replace_all(line,dic)
-        line=cleaning(line)
-        #line=remove_tags(line)
-        print(line.split())
-
-    return None
-
-
-
-
-def splitfichier(name):
-    r=open("out2.xml",'w')
-    i=0
+def nettoyage(name):
+    stop_words = set(stopwords.words('french'))
+    print(stop_words)
     for line in open(name,'r'):
-        if i<2383:
-            r.write(line)
-            i+=1
+        line =cleaning(line)
+        word_tokens = word_tokenize(line)
+        filtered_sentence = [w for w in word_tokens if not w in stop_words]
+        filtered_sentence = []
+        for w in word_tokens:
+            if w not in stop_words:
+                filtered_sentence.append(w)
+        print(filtered_sentence)
     return None
 
-#print(reduire_mot_cle("testfichier.xml", "musique", "out.xml"))
-
-#nombres de pages
-def count_page(name_file):
-    return len(ET.parse(name_file).getroot().findall(pref+'page'))
-
-
-print(count_page("out.xml"))
-print(count_page("testfichier.xml"))
-print(dictionnaire_de_mot("out2.xml"))
+#print(testReduire())
+print(nettoyage("outfevrier.xml"))
+#print(ouvrir2efile())
